94656b8e18b4eeebb1b228b4caeb6aba8a13d2bf
LGatodu47 <62804774+LGatodu47@users.noreply.github.com>
2023-07-13 10:02:38 +0200

    Changed mixin and renamed font files to lowercase

--- src/main/java/io/github/mineria_mc/mineria/mixin/CreativeModeTabsMixin.java
+++ src/main/java/io/github/mineria_mc/mineria/mixin/CreativeModeTabsMixin.java
@@ -1,33 +1,28 @@
 package io.github.mineria_mc.mineria.mixin;
 
 import io.github.mineria_mc.mineria.common.init.MineriaPotions;
-import io.github.mineria_mc.mineria.util.DeferredRegisterUtil;
 import io.github.mineria_mc.mineria.util.MineriaUtils;
 import net.minecraft.core.Holder;
+import net.minecraft.core.HolderLookup;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.alchemy.Potion;
-import net.minecraft.world.item.alchemy.PotionUtils;
-import net.minecraftforge.registries.RegistryObject;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 import java.util.Set;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @Mixin(CreativeModeTabs.class)
 public class CreativeModeTabsMixin {
+    @Unique
     private static final Set<ResourceKey<Potion>> MINERIA_POTIONS = MineriaPotions.POTIONS.getEntries().stream().map(MineriaUtils::key).collect(Collectors.toSet());
 
-    @Inject(method = "lambda$generatePotionEffectTypes$35", at = @At("HEAD"), cancellable = true)
-    private static void mineria$inject_filterPotions(Holder.Reference<Potion> potion, CallbackInfoReturnable<Boolean> cir) {
-        if(potion.is(MINERIA_POTIONS::contains)) {
-            cir.setReturnValue(false);
-        }
+    @Redirect(method = "generatePotionEffectTypes", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/HolderLookup;listElements()Ljava/util/stream/Stream;"))
+    private static Stream<Holder.Reference<Potion>> mineria$redirect_listElements(HolderLookup<Potion> lookup) {
+        return lookup.listElements().filter(potion -> !potion.is(MINERIA_POTIONS::contains));
     }
 }
